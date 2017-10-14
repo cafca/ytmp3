539d87522bd38483be91fd85cd78947858cacf0b
Vincent Ahrend <nichte@gmail.com>
2017-10-14 16:22:43 +0200

    Add dist to repo

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 
 # Run from git root directory
 
-pyinstaller -F --distpath="../dist" --workpath="../build" -n "ytmp3" ytmp3.py
+pyinstaller -F --workpath="../build" -n "ytmp3" ytmp3.py
 rm -rf __pycache__
